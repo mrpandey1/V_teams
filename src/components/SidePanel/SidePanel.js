4ed96edcb9af715e6220b5f03e6af76eb7916eda
mrpandey1 <pandeynishant525@gmail.com>
2021-08-31 14:58:07 +0530

    starred channels

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,6 +1,7 @@
 import React from "react";
 import UserPanel from "./UserPanel";
 import { Menu } from "semantic-ui-react";
+import Starred from "./Starred";
 class SidePanel extends React.Component {
   render() {
     const { currentUser,primaryColor } = this.props;
@@ -14,6 +15,7 @@ class SidePanel extends React.Component {
         style={{ background: primaryColor, fontSize: "1.2rem" }}
       >
         <UserPanel primaryColor={primaryColor} currentUser={currentUser} />
+        <Starred currentUser={currentUser} />
       </Menu>
     );
   }
